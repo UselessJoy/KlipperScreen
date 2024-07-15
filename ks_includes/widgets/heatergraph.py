d82f0771b4ba61cbe096da87c1e217167b7344be
UselessJoy <mrqwer2014@mail.ru>
2024-07-15 09:03:43 +0300

    Update heatergraph.py
    Fix fullscreen graph dialog

--- ks_includes/widgets/heatergraph.py
+++ ks_includes/widgets/heatergraph.py
@@ -36,7 +36,7 @@ class HeaterGraph(Gtk.DrawingArea):
 
     def show_fullscreen_graph(self):
         self.fs_graph = HeaterGraph(self._screen, self.printer, self.font_size * 2, fullscreen=True, store=self.store)
-        self._gtk.Dialog(_("Temperature"), None, self.fs_graph, self.close_fullscreen_graph)
+        self._gtk.Dialog(None, self.fs_graph, _("Temperature"), self.close_fullscreen_graph)
 
     def close_fullscreen_graph(self, dialog, response_id):
         logging.info("Closing graph")
